b09be38a9c10f3d03e69267eb8efacc2c3de7e26
スペンサー <spencerpomme@live.com>
2017-11-05 11:04:33 +0800

    First draft of k-means implementation

--- assignment/assignment_2/k-means.py
+++ assignment/assignment_2/k-means.py
@@ -25,31 +25,31 @@ def kmeans(dataset, k=2):
     :param k: number of desired clusters
     :return: a 3d list containing partitioned dataset
     """
-    k_clusters = []
-    last_round_mean = []
-    curr_round_mean = []
+    k_clusters = [] # this list will include id number (ref) for each point
+    last_round_means = []
+    curr_round_means = []
     for i in range(k):
         k_clusters.append([dataset[i]])
-        curr_round_mean.append(dataset[i])
-    random_ptrs = [item for item in dataset if item not in curr_round_mean]
+        curr_round_means.append(dataset[i][1:]) # the [1:] is to exclude the ref(id)
+    random_ptrs = [item for item in dataset if item not in curr_round_means]
     print("current k clusters:", k_clusters)
-    print("curr_round_mean:", curr_round_mean)
+    print("curr_round_mean:", curr_round_means)
 
-    while is_different(last_round_mean, curr_round_mean):
-        last_round_mean = curr_round_mean
+    while is_different(last_round_means, curr_round_means):
+        last_round_means = curr_round_means
         for rdpt in random_ptrs:
             # pt is a list (a row of dataset)
-            min_dist = dist(rdpt, curr_round_mean[0])
-            curr_closest_cluster = 0  # the subfix of current closest cluster mean among k clusters
-            for i in range(len(curr_round_mean)):
+            min_dist = dist(rdpt[1:], curr_round_means[0]) # the [1:] is to exclude the ref(id)
+            curr_closest_cluster = 0  # the subfix of current closest cluster mean among k clusters, initially set 0
+            for i in range(len(curr_round_means)):
 
-                curr_dist = dist(rdpt, curr_round_mean[i])
-                if curr_dist < dist(rdpt, curr_round_mean[0]):
+                curr_dist = dist(rdpt[1:], curr_round_means[i]) # the [1:] is to exclude the ref(id)
+                if curr_dist < dist(rdpt[1:], curr_round_means[0]):
                     curr_closest_cluster = i
                     min_dist = curr_dist
             k_clusters[curr_closest_cluster].append(rdpt)
         # Need to update {last_round_mean and curr_round_mean
-        curr_round_mean = update_mean(k_clusters) # TODO
+        curr_round_means = update_mean(k_clusters)
 
 
 def is_different(l1, l2):
@@ -81,6 +81,32 @@ def dist(p1: list, p2: list) -> int:
         distance += (p1[i] - p2[i]) ** 2
     return distance
 
+def update_mean(clusters: list) -> list:
+    """
+    Update the mean of current cluster.
+    :param clusters: The k cluster 3d list
+    :return: A 2d list, the new mean of each cluster, update the value of curr_round_mean
+    """
+    new_means = []
+    for cluster in clusters:
+        new_means.append(mean(cluster))
+    return new_means
+
+def mean(cluster):
+    """
+    Calculate the mean point of a lot of points.
+    :param cluster: 2d list of a cluster (a list of points)
+    :return: 1d list representing the new mean (a point)
+    """
+    new_mean = [0] * (len(cluster[0][0]) - 1)
+    for pt in cluster:
+        for i in range(len(pt)):
+            new_mean[i] += pt[i+1]
+    return [val / len(cluster) for val in new_mean]
+
+
+
+
 
 if __name__ == "__main__":
     """
@@ -111,4 +137,5 @@ if __name__ == "__main__":
                range(len(ref))]
     pprint(dataset)
 
-    kmeans(dataset)
+    res_clusters = kmeans(dataset, k=2)
+    print(res_clusters)
